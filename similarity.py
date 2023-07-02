4273364c6e7749d535076fbe70640bf8484c3707
nick01as <77217356+nick01as@users.noreply.github.com>
2023-07-02 00:09:26 -0700

    Update similarity.py

--- similarity.py
+++ similarity.py
@@ -66,8 +66,7 @@ def soft_wpmi(clip_feats, target_feats, target_neuron, top_k=100, a=10, lam=1, d
         print("clip_feats shape: {}".format(clip_feats.shape))
         print("inds shape: {}".format(inds.shape))
         print("curr_clip shape: {}".format(curr_clip_feats.shape))
-        for i in range(3):
-            print(curr_clip_feats[:,i:i+1])
+        print(inds[:,0])
         
         curr_p_d_given_e = 1+p_in_examples*(curr_clip_feats-1)
         curr_p_d_given_e = torch.sum(torch.log(curr_p_d_given_e+min_prob), dim=0, keepdim=True)
